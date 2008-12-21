ae3ff3c60fb169cda33abed77e54a583af07b381
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-21 19:15:32 +0000

    Выложена версия 0.03 плагина feedback:
    [0.03][2008-12-21]
    + Добавлено описание плагина (readme)
    + Добавлена поддержка персональных шаблонов для плагина
    * Перенесены все файлы изображений в собственный каталог плагина. Предыдущие версии были адаптированы только для работы с шаблоном default.
    # [ВНИМАНИЕ] Необходимо следовать процедуре обновления (upgrade) в случае, если у вас уже установлена более старая версия плагина.

---
+++ ng_plugins/trunk/feedback/uninstall.php
@@ -0,0 +1,29 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+//
+// Configuration file for plugin
+//
+
+plugins_load_config();
+loadPluginLang('feedback', 'config', '', '', ':');
+
+
+$db_update = array(
+	array(
+		'table'		=>	'feedback',
+		'action'	=>	'drop',
+	),
+);
+
+if ($_REQUEST['action'] == 'commit') {
+	if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
+		plugin_mark_deinstalled($plugin);
+	}
+} else {
+	generate_install_page($plugin, $lang['feedback:text.deinstall'], 'deinstall');
+}
+
+?>
