832cff12bab5fd1fde1c1e3f438751e6a67d3ece
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-25 22:35:06 +0000

    Адаптация плагина simple_title для работы с NGCMS

--- simple_title/config.php
+++ simple_title/config.php
@@ -1,8 +1,8 @@
 <?php
 plugins_load_config();
 $cfg = array();
-array_push($cfg, array('name' => 'c_title', 'title' => 'Title для категории<br>Разрешено только %1% и %3%','type' => 'input', 'value' => extra_get_param('simple_title','c_title')));
-array_push($cfg, array('name' => 'n_title', 'title' => 'Title для новости<br>Все три разрешены','type' => 'input', 'value' => extra_get_param('simple_title','n_title')));
+array_push($cfg, array('name' => 'c_title', 'title' => 'Title для категории', 'descr' => 'Разрешено только %1% и %3%','type' => 'input', 'value' => extra_get_param('simple_title','c_title')));
+array_push($cfg, array('name' => 'n_title', 'title' => 'Title для новости', 'descr' => 'Все три разрешены','type' => 'input', 'value' => extra_get_param('simple_title','n_title')));
 array_push($cfg, array('name' => 'm_title', 'title' => 'Title для главной страницы<br>Только %3%','type' => 'input', 'value' => extra_get_param('simple_title','m_title')));
 array_push($cfg, array('name' => 'static_title', 'title' => 'Title для статической страницы<br>Только %3%','type' => 'input', 'value' => extra_get_param('simple_title','static_title')));
 
