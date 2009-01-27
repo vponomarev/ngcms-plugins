e7a3bb29cc7c6b69dc52543bb2c8d648f75b1da8
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-27 23:06:55 +0000

    [DEVEL версия] Продолжение работы над комментариями.

--- ng_plugins/trunk/comments/comments.php
+++ ng_plugins/trunk/comments/comments.php
@@ -80,7 +80,19 @@ function plugin_comments_add() {
 
 	// Call comments_add() to ADD COMMENT
 	if (is_array($addResult = comments_add())) {
-		// OK. Let's print new comment
+
+		// Ok.
+		// Check if AJAX mode is turned OFF
+		if (!$_REQUEST['ajax']) {
+			// Make FULL news link
+			$link = GetLink('full', $addResult[0]);
+			// Make redirect to full news
+			@header("Location: ".$link);
+			return 1;
+		}
+
+		// AJAX MODE.
+		// Let's print (ONLY) new comment
 		$SQLnews	= $addResult[0];
 		$commentId	= $addResult[1];
 
@@ -102,7 +114,7 @@ function plugin_comments_add() {
 		comments_show($SQLnews['id'], $commentId, $SQLnews['com']+1, $callingCommentsParams);
 		return 1;
 	} else {
-		// Some errors
+		// Some errors.
 		$tpl -> template('error', tpl_site);
 		$tpl -> vars('error', array( 'vars' => array('content' => $template['vars']['mainblock'])));
 		echo $tpl -> show('error');
@@ -113,7 +125,7 @@ function plugin_comments_add() {
 
 // Show dedicated page for comments
 function plugin_comments_show(){
-	global $config, $catz, $mysql, $catmap, $tpl, $template, $SUPRESS_TEMPLATE_SHOW;
+	global $config, $catz, $mysql, $catmap, $tpl, $template, $SUPRESS_TEMPLATE_SHOW, $userROW;
 
 	include_once(root."/plugins/comments/inc/comments.show.php");
 
@@ -126,47 +138,71 @@ function plugin_comments_show(){
 	}
 
 	// Prepare params for call
-	$callingCommentsParams = array();
+	$callingCommentsParams = array( 'noajax' => 1);
 
 	// Set default template path
 	$templatePath = tpl_dir.$config['theme'];
 
-	$fcat = array_shift(explode(",", $SQLnews['catid']));
+	$fcat = array_shift(explode(",", $newsRow['catid']));
 
 	// Check if there is a custom mapping
 	if ($fcat && $catmap[$fcat] && ($ctname = $catz[$catmap[$fcat]]['tpl'])) {
 		// Check if directory exists
 		if (is_dir($templatePath.'/ncustom/'.$ctname))
 			$callingCommentsParams['overrideTemplatePath'] = $templatePath.'/ncustom/'.$ctname;
+			$templatePath = $templatePath.'/ncustom/'.$ctname;
 	}
 
+	// Show header file
+	$tvars = array();
+	$tvars['vars']['link']	= GetLink('full', $newsRow);
+	$tvars['vars']['title']	= secure_html($newsRow['title']);
+
+	$tpl->template('comments.header', $templatePath);
+	$tpl->vars('comments.header', $tvars);
+
+	$template['vars']['mainblock'] .= $tpl->show('comments.header');
+
+	// Show comments
 	include_once(root."/plugins/comments/inc/comments.show.php");
 
 	// Check if we need pagination
-	$flagMoreComments = 0;
+	$flagMoreComments	= 0;
+	$page				= 0;
+	$pageCount			= 0;
 
 	if (extra_get_param('comments', 'multipage')) {
 		// If we have comments more than for one page - activate pagination
-		if ((intval(extra_get_param('comments', 'multi_mcount')) > 0) && ($SQLnews['com'] > intval(extra_get_param('comments', 'multi_mcount')))) {
-			$callingCommentsParams['limitCount'] = intval(extra_get_param('comments', 'multi_mcount'));
+		if ((intval(extra_get_param('comments', 'multi_mcount')) > 0) && ($newsRow['com'] > intval(extra_get_param('comments', 'multi_mcount')))) {
 			$flagMoreComments = 1;
-		}
 
+			// Page count
+			$pageCount = ceil($newsRow['com']/intval(extra_get_param('comments', 'multi_scount')));
+
+			// Check if user wants to access not first page
+			$page = intval($_REQUEST['page']);
+			if ($page < 1) $page = 1;
+
+			$callingCommentsParams['limitCount'] = intval(extra_get_param('comments', 'multi_scount'));
+			$callingCommentsParams['limitStart'] = ($page-1) * intval(extra_get_param('comments', 'multi_scount'));
+		}
 	}
 
 	// Show comments
 	comments_show($newsID, 0, 0, $callingCommentsParams);
 
-	// If multipage is used and we have more comments - show
-	if ($flagMoreComments) {
+	if ($pageCount > 1) {
 		$link = GetLink('plugins', array('plugin_name' => 'comments'), 1);
-		$link .= ((strpos($link,'?') === false)?'?':'&').'plugin_cmd=show&news_id='.$newsID;
-		$template['vars']['mainblock'] .= str_replace('{link}', $link, $lang['comments:link.more']);
+		$link .= ((strpos($link,'?') === false)?'?':'&').'plugin_cmd=show&news_id='.$newsID.'&page={page}';
+
+		$navigations = getNavigations(tpl_dir.$config['theme']);
+		$template['vars']['mainblock'] .= generatePagination($page, 1, $pageCount, 10, $link, $navigations);
 	}
 
 	// Show form for adding comments
-	if ($SQLnews['allow_com'] && (!extra_get_param('comments', 'regonly') || is_array($userROW)))
+	if ($newsRow['allow_com'] && (!extra_get_param('comments', 'regonly') || is_array($userROW))) {
 		comments_showform($newsID, $callingCommentsParams);
+	}
 
 }
 
