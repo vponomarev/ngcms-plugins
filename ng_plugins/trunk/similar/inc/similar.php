d74cb6b7bf3712b6fa7d2d88ff93bafbfaac7a9f
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-02-09 21:56:24 +0000

    Выложена обновлённая (0.03) версия плагина similar:
    [0.03][2009-02-06]
    * Изменена логика формирования списка "похожих" новостей.
      В предыдущей версии всегда поддерживалась полная база похожести. И при редактировании новостей создавалась высокая нагрузка на БД.
      В текущей версии у новости есть статус "BROKEN", который означает, что необходимо обновить список похожих новостей при показе текущей новости.

--- ng_plugins/trunk/similar/inc/similar.php
+++ ng_plugins/trunk/similar/inc/similar.php
@@ -1,7 +1,50 @@
 <?php
 
+//
+// Recover SIMILAR data for selected news
+// [ RECOVER is called directly before news is showed ]
+function plugin_similar_recover($newsID, $count) {
+	global $mysql;
 
-function plugin_similar_repopulate($newsid, $count, $skipndel = array()){
+//	print "call REGENERATE SIMILARS for NEWS: ".$newsID." (".$count.")<br/>\n";
+	if (!$count)
+		return 0;
+
+	// Delete old data
+	$mysql->query("delete from ".prefix."_similar_index where newsID = ".db_squote($newsID));
+
+	// Load a list of similar looking news via TAG index
+	$list = $mysql->select("select i.newsID, count(i.tagID) as cnt from ".prefix."_tags_index i where (i.newsID <> ".db_squote($newsID).") and (i.tagID in ( select tagID from ".prefix."_tags_index where newsID = ".db_squote($newsID).") ) group by i.newsID order by cnt desc limit ".intval($count));
+
+	// Check if we have something similar-looking. Break if nothing similar
+	if (!sizeof($list)) {
+		$mysql->query("update ".prefix."_news set similar_status = 1 where id = ".db_squote($newsID));
+		// Return: OK, news do not have any similars
+		return 1;
+	}
+
+	// Fine. Now we have a list of similar news.
+	// Let's load data from this news
+	$nlist = array();
+	foreach ($list as $sr)
+		$nlist [$sr['newsID']] = $sr['cnt'];
+
+	$nl = $mysql->select("select id, title, editdate, postdate from ".prefix."_news where id in (".join(", ", array_keys($nlist)).")"); 
+
+	// Now we have everything we need. Let's update similar list
+	foreach ($nl as $nrow) {
+		$mysql->query("insert into ".prefix."_similar_index (newsID, refNewsID, refNewsQuantaty, refNewsTitle, refNewsDate) values (".
+			db_squote($newsID).", ".db_squote($nrow['id']).", ".db_squote($nlist[$nrow['id']]).", ".db_squote($nrow['title']).", ".db_squote($nrow['editdate']?$nrow['editdate']:$nrow['postdate']).")");
+	}
+
+	// And at the end - update news status
+	$mysql->query("update ".prefix."_news set similar_status = 2 where id = ".db_squote($newsID));
+
+	// Return: OK, news have similars
+	return 2;
+}
+
+function plugin_similar_repopulate($newsid, $count){
 	global $mysql;
 
 	$affectedList = array();
@@ -17,10 +60,10 @@ function plugin_similar_repopulate($newsid, $count, $skipndel = array()){
 		$ld [] = db_squote($newsID);
 
 	// Delete old rec's
-	$mysql->query("delete from ".prefix."_similar_index where (newsID in (". join(", ", $ld).") or refNewsID in (". join(", ", $ld)."))".(count($skipndel)?' and NewsId not in ('.join(", ", $skipndel).')':''));
+	$mysql->query("delete from ".prefix."_similar_index where newsID in (". join(", ", $ld).") or refNewsID in (". join(", ", $ld).")");
 
 	foreach ($newsIDlist as $newsID) {
-		$list = $mysql->select("select i.newsID, count(i.tagID) as cnt, n.title, n.editdate, n.postdate from ".prefix."_tags_index i left join ".prefix."_news n on n.id = i.newsID where (i.newsID <> ".db_squote($newsID).") and (i.tagID in ( select tagID from ".prefix."_tags_index use index(newsID) where newsID = ".db_squote($newsID).") ) group by newsID order by cnt desc");
+		$list = $mysql->select("select i.newsID, count(i.tagID) as cnt, n.title, n.editdate, n.postdate from ".prefix."_tags_index i left join ".prefix."_news n on n.id = i.newsID where (i.newsID <> ".db_squote($newsID).") and (i.tagID in ( select tagID from ".prefix."_tags_index where newsID = ".db_squote($newsID).") ) group by newsID order by cnt desc");
 
 		// Populate data for our news
 		for ($i = 0; $i < min(count($list), $count); $i++) {
--- ng_plugins/trunk/similar/install.php
+++ ng_plugins/trunk/similar/install.php
@@ -10,6 +10,17 @@ LoadPluginLang($plugin, 'main');
 
 $db_update = array(
 	array(
+		'table'  => 'news',
+		'action' => 'cmodify',
+		'fields' => array(
+		// Status of similar news:
+		// 0 - BROKEN (data should be rebuilded)
+		// 1 - No SIMILAR data
+		// 2 - Have SIMILAR data
+		array('action' => 'cmodify', 'name' => 'similar_status', 'type' => 'int', 'params' => "default 0"),
+		)
+	),
+	array(
 		'table'		=>	'similar_index',
 		'action'	=>	'cmodify',
 		'key'		=>	'primary key(`id`), key `newsID` (`newsID`) ',
@@ -21,7 +32,7 @@ $db_update = array(
 			array('action' => 'cmodify', 'name' => 'refNewsQuantaty',  'type' => 'int'),
 			array('action' => 'cmodify', 'name' => 'refNewsTitle',  'type' => 'varchar(120)', 'params' => ''),
 			array('action' => 'cmodify', 'name' => 'refNewsDate',  'type' => 'int'),
-		)                                                    	
+		)
 	),
 );
 
@@ -29,7 +40,7 @@ $db_update = array(
 if ($_REQUEST['action'] == 'commit') {
 	if (fixdb_plugin_install($plugin, $db_update)) {
 		plugin_mark_installed($plugin);
-	}	
+	}
 } else {
 	$text = 'ѕлагин <b>similar</b> позвол€ет создавать блоки похожих (по различным пол€м) новостей.<br />Ќа текущий момент поддерживаетс€ только анализ тегов.';
 	generate_install_page($plugin, $text);
