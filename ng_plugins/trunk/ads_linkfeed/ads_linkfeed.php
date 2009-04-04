923ee7ff7cc0cc74f9e74f4791eef9f16d7175d0
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-04-04 21:43:24 +0000

    Выложен новый плагин ads_linkfeed (версия 0.01)

---
+++ ng_plugins/trunk/ads_linkfeed/ads_linkfeed.php
@@ -0,0 +1,45 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+if (!defined('LINKFEED_USER')){
+	define('LINKFEED_USER', extra_get_param('ads_linkfeed', 'linkfeed_user'));
+	include_once(root."/plugins/ads_linkfeed/inc/linkfeed.php");
+}
+
+
+add_act('index_post', 'plugin_ads_linkfeed');
+
+function plugin_ads_linkfeed() {
+	global $template;
+
+	$count = abs(intval(extra_get_param('ads_linkfeed', 'bcount')));
+	if (!$count) $count = 1;
+
+	$blen = array();
+	foreach (split(",", extra_get_param('ads_linkfeed', 'blength')) as $br) {
+		$blen []= intval(trim($br));
+	}
+
+	$flag_final   = false;
+	$blen[$count] = 0;
+
+	$linkfeed = new LinkfeedClient(array(
+		'multi_site' => extra_get_param('ads_linkfeed', 'multisite')?true:false,
+		'db_dir' => get_plugcache_dir('ads_linkfeed'),
+	));
+	
+	for ($i = 1; $i <= $count; $i++) {
+		if ($flag_final) {
+			$template['vars']['plugin_ads_linkfeed_'.$i] = '';
+		} else {
+			if (!$blen[$i-1]) {
+				$template['vars']['plugin_ads_linkfeed_'.$i] = $linkfeed->return_links($blen[$i-1]);
+			} else {
+				$flag_final = true;
+				$template['vars']['plugin_ads_linkfeed_'.$i] = $linkfeed->return_links();
+			}
+		}
+	}	
+}
