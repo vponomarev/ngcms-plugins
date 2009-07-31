fd2726d1d4ddfe4d1c18c256cd8ee520dafd6747
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-07-31 22:39:27 +0000

    % Добавлен деинсталлер для плагина jchat

---
+++ ng_plugins/trunk/jchat/deinstall.php
@@ -0,0 +1,29 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+//
+// Configuration file for plugin
+//
+
+plugins_load_config();
+loadPluginLang('jchat', 'config', '', '', ':');
+
+
+$db_update = array(
+	array(
+		'table'		=>	'jchat',
+		'action'	=>	'drop',
+	),
+);
+
+if ($_REQUEST['action'] == 'commit') {
+	if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
+		plugin_mark_deinstalled($plugin);
+	}
+} else {
+	generate_install_page('jchat', $lang['jchat:desc_deinstall'], 'deinstall');
+}
+
+
