e374d4b826bb90247703ace9814cb27b7216e01d
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-11-27 23:12:15 +0000

    + Выложена обновлённая (0.17) версия плагина auth_basic: [0.17][2009-11-28] + Добавлена возможность обновлять параметр "последний вход" для пользователей во время их работы с сайтом (максимальная частота обновлений задаётся в секундах). В предыдущей версии в поле "последний вход" хранилась дата последней авторизации.

--- auth_basic/config.php
+++ auth_basic/config.php
@@ -20,15 +20,17 @@ $regstatus = intval(extra_get_param('auth_basic','regstatus'));
 if (($regstatus < 1)||($regstatus > 4))
  $regstatus = 4;
 
+$lastupdate = intval(pluginGetVariable('auth_basic', 'lastupdate'));
+if ($lastupdate<1) { $lastupdate = ''; }
+
 // Fill configuration parameters
 $cfg = array();
 array_push($cfg, array('descr' => $lang['auth:description']));
+array_push($cfg, array('name' => 'lastupdate', 'title' => $lang['auth:lastupdate'], 'descr' => $lang['auth:lastupdate_descr'],'type' => 'input', value => $lastupdate));
 array_push($cfg, array('name' => 'regstatus', 'title' => $lang['auth:regstatus'], 'descr' => $lang['auth:regstatus_descr'],'type' => 'select',  'values' => array ( '1' => '1 - '.$lang['st_1'], '2' => '2 - '.$lang['st_2'], '3' => '3 - '.$lang['st_3'], '4' => '4 - '.$lang['st_4']), value => $regstatus));
 array_push($cfg, array('name' => 'restorepw', 'title' => $lang['auth:restorepw'], 'descr' => $lang['auth:restorepw_descr'],'type' => 'select',  'values' => array ( '0' => $lang['auth:restore_disabled'], 'login' => $lang['auth:restore_login'], 'email' => $lang['auth:restore_email'], 'both' => $lang['auth:restore_both']), value => extra_get_param('auth_basic','restorepw')));
 array_push($cfg, array('name' => 'en_dbprefix', 'title' => $lang['auth:en_dbprefix'], 'descr' => $lang['auth:en_dbprefix_descr'],'type' => 'checkbox', value => extra_get_param('auth_basic','en_dbprefix')));
-array_push($cfg, array('name' => 'dbprefix', 'title' => $lang['auth:dbprefix'], 'descr' => $lang['auth:dbprefix_descr'],'type' => 'text', value => extra_get_param('auth_basic','dbprefix')));
-
-//array_push($cfg, array('name' => 'multilogin', title => 'Разрешить многократный вход', 'descr' => 'возможность работы с нескольких мест одновременно.<br />!!! снижает производительность !!!','type' => 'select', 'values' => array ( '0' => 'нет', '1' => 'да'), value => extra_get_param('auth_basic','multilogin')));
+array_push($cfg, array('name' => 'dbprefix', 'title' => $lang['auth:dbprefix'], 'descr' => $lang['auth:dbprefix_descr'],'type' => 'input', value => extra_get_param('auth_basic','dbprefix')));
 
 
 // RUN 
@@ -40,5 +42,3 @@ if ($_REQUEST['action'] == 'commit') {
 	generate_config_page('auth_basic', $cfg);
 }
 
-
-?>
