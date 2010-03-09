7ee881d1f2531d92840173da0b5c089cd2922210
Amarelius <Amarelius@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-03-09 12:18:41 +0000

    % Теперь при работе с плагином re_stat в title не отображается слово "плагин"

--- ng_plugins/trunk/re_stat/re_stat.php
+++ ng_plugins/trunk/re_stat/re_stat.php
@@ -8,7 +8,8 @@ foreach ($re_stat_values as $key => $row) register_plugin_page('re_stat', $row['
 
 function plugin_re_stat($params)
 {
-	global $CurrentHandler;
+	global $CurrentHandler, $SYSTEM_FLAGS;
+	unset($SYSTEM_FLAGS['info']['title']['group']);
 	$idstat = 0;
 	$values = pluginGetVariable('re_stat', 'values');
 	foreach ($values as $key => $row) if ($row['code'] == $CurrentHandler['handlerName']) {$idstat = $row['id']; break;}
