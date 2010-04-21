de63284d53a683494daae4f578832eccbcf20a02
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-04-21 23:36:17 +0000

    Выложена обновлённая (0.05) версия плагина archive: [0.05][2010-04-22] + Добавлена поддержка текстового описания количества новостей

--- ng_plugins/trunk/archive/archive.php
+++ ng_plugins/trunk/archive/archive.php
@@ -5,19 +5,23 @@ if (!defined('NGCMS')) die ('HAL');
 
 add_act('index', 'plugin_archive');
 
+// Load lang file only if text counter is enabled
+if (pluginGetVariable('archive', 'tcounter'))
+	LoadPluginLang('archive', 'main', '', '', ':');
+
 
 function plugin_archive() {
-	global $config, $mysql, $tpl, $template, $langMonths;
+	global $config, $mysql, $tpl, $template, $langMonths, $lang;
 
-	$maxnum    = intval(extra_get_param('archive','maxnum'));
-	$counter   = intval(extra_get_param('archive','counter'));
+	$maxnum    = intval(pluginGetVariable('archive','maxnum'));
+	$counter   = intval(pluginGetVariable('archive','counter'));
 
 	if (($maxnum < 1) || ($maxnum > 50)) $maxnum = 12;
 
 	// Generate cache file name [ we should take into account SWITCHER plugin ]
 	$cacheFileName = md5('archive'.$config['theme'].$config['default_lang']).'.txt';
 
-	if (extra_get_param('archive','cache')) {
+	if (pluginGetVariable('archive','cache')) {
 		$cacheData = cacheRetrieveFile($cacheFileName, extra_get_param('archive','cacheExpire'), 'archive');
 		if ($cacheData != false) {
 			// We got data from cache. Return it and stop
@@ -25,6 +29,9 @@ function plugin_archive() {
 			return;
 		}
 	}
+
+	// Load list
+	$caseList = explode(',', $lang['archive:counter.case']);
 
 	// Determine paths for all template files
 	$tpath = locatePluginTemplates(array('entries', 'archive'), 'archive', extra_get_param('archive', 'localsource'));
@@ -35,11 +42,40 @@ function plugin_archive() {
 					generateLink('news', 'by.month', array('year' => $row['year'], 'month' => sprintf('%02u', $row['month']))):
 					generateLink('core', 'plugin', array('plugin' => 'news', 'handler' => 'by.month'), array('year' => $row['year'], 'month' => sprintf('%02u', $row['month'])));
 
+
 		$tvars['vars'] = array(
 			'link'		=>	$month_link,
 			'title'		=>	$langMonths[$row['month']-1].' '.$row['year'],
-			'cnt'		=>	$row['cnt']
+			'cnt'		=>	$row['cnt'],
 		);
+
+		if (pluginGetVariable('archive', 'tcounter')) {
+			// Determine current case
+			$sCase = 99;
+			$cnt = $row['cnt'];
+			if ($cnt == 1) {
+				$sCase = 1;
+			} else if (($cnt >= 2) && ($cnt <= 4)) {
+				$sCase = 2;
+			} else if (($cnt >= 5) && ($cnt <= 20)) {
+				$sCase = 4;
+			} else {
+				$tsCase = $sCase % 10;
+				if ($tsCase == 0) {
+					$sCase = 4;
+				} else if ($tsCase == 1) {
+					$sCase = 1;
+				} else if (($tsCase >= 2) && ($tsCase <= 4)) {
+					$sCase = 2;
+				} else {
+					$sCase = 4;
+				}
+			}
+			$tvars['vars']['ctext'] = $caseList[$sCase-1];
+		} else {
+			$tvars['vars']['ctext'] = '';
+		}
+
 		$tvars['regx']["'\[counter\](.*?)\[/counter\]'si"] = $counter?'$1':'';
 
 		$tpl -> template('entries', $tpath['entries']);
--- ng_plugins/trunk/archive/config.php
+++ ng_plugins/trunk/archive/config.php
@@ -9,27 +9,29 @@ if (!defined('NGCMS')) die ('HAL');
 
 // Preload config file
 plugins_load_config();
+LoadPluginLang('archive', 'config', '', '', ':');
 
 
 // Fill configuration parameters
 $cfg = array();
 $cfgX = array();
-array_push($cfg, array('descr' => 'Плагин отображает "архив сайта" - список месяцев в которых были размещены новости на сайте. При клике на конкретный месяц будут отображаться новости за этот месяц'));
-array_push($cfgX, array('name' => 'maxnum', 'title' => "Ограничение кол-ва отображаемых месяцев", 'descr' => 'используется для сайтов с большой историей и позволяет не &quot;раздувать&quot; блок архива до огромных размеров', 'type' => 'input', 'value' => intval(extra_get_param($plugin,'maxnum'))?extra_get_param($plugin,'maxnum'):'12'));
-array_push($cfgX, array('name' => 'counter', 'title' => "Отображать счетчик новостей<br /><b>Да</b> - счетчик будет отображаться<br /><b>Нет</b> - счетчик не будет отображаться", 'type' => 'select', 'values' => array ( '0' => 'Нет', '1' => 'Да'), 'value' => intval(extra_get_param($plugin,'counter'))));
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>Настройки плагина</b>', 'entries' => $cfgX));
+array_push($cfg, array('descr' => $lang['archive:description']));
+array_push($cfgX, array('name' => 'maxnum', 'title' => $lang['archive:maxnum'], 'descr' => $lang['archive:maxnum#desc'], 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'maxnum'))?pluginGetVariable($plugin,'maxnum'):'12'));
+array_push($cfgX, array('name' => 'counter', 'title' => $lang['archive:counter'], 'descr' => $lang['archive:counter#desc'], 'type' => 'select', 'values' => array ( '0' => 'Нет', '1' => 'Да'), 'value' => intval(pluginGetVariable($plugin,'counter'))));
+array_push($cfgX, array('name' => 'tcounter', 'title' => $lang['archive:tcounter'], 'descr' => $lang['archive:tcounter#desc'], 'type' => 'select', 'values' => array ( '0' => 'Нет', '1' => 'Да'), 'value' => intval(pluginGetVariable($plugin,'tcounter'))));
+array_push($cfg,  array('mode' => 'group', 'title' => $lang['archive:group.config'], 'entries' => $cfgX));
 
 $cfgX = array();
-array_push($cfgX, array('name' => 'localsource', 'title' => "Выберите каталог из которого плагин будет брать шаблоны для отображения<br /><small><b>Шаблон сайта</b> - плагин будет пытаться взять шаблоны из общего шаблона сайта; в случае недоступности - шаблоны будут взяты из собственного каталога плагина<br /><b>Плагин</b> - шаблоны будут браться из собственного каталога плагина</small>", 'type' => 'select', 'values' => array ( '0' => 'Шаблон сайта', '1' => 'Плагин'), 'value' => intval(extra_get_param($plugin,'localsource'))));
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>Настройки отображения</b>', 'entries' => $cfgX));
+array_push($cfgX, array('name' => 'localsource', 'title' => $lang['archive:localsource'], 'descr' => $lang['archive:localsource#desc'], 'type' => 'select', 'values' => array ( '0' => 'Шаблон сайта', '1' => 'Плагин'), 'value' => intval(pluginGetVariable($plugin,'localsource'))));
+array_push($cfg,  array('mode' => 'group', 'title' => $lang['archive:group.source'], 'entries' => $cfgX));
 
 $cfgX = array();
-array_push($cfgX, array('name' => 'cache', 'title' => "Использовать кеширование данных<br /><small><b>Да</b> - кеширование используется<br /><b>Нет</b> - кеширование не используется</small>", 'type' => 'select', 'values' => array ( '1' => 'Да', '0' => 'Нет'), 'value' => intval(extra_get_param($plugin,'cache'))));
-array_push($cfgX, array('name' => 'cacheExpire', 'title' => "Период обновления кеша<br /><small>(через сколько секунд происходит обновление кеша. Значение по умолчанию: <b>60</b>)</small>", 'type' => 'input', 'value' => intval(extra_get_param($plugin,'cacheExpire'))?extra_get_param($plugin,'cacheExpire'):'60'));
-array_push($cfg,  array('mode' => 'group', 'title' => '<b>Настройки кеширования</b>', 'entries' => $cfgX));
+array_push($cfgX, array('name' => 'cache', 'title' => $lang['archive:cache'], $lang['archive:cache#desc'], 'type' => 'select', 'values' => array ( '1' => 'Да', '0' => 'Нет'), 'value' => intval(pluginGetVariable($plugin,'cache'))));
+array_push($cfgX, array('name' => 'cacheExpire', 'title' => $lang['archive:cacheExpire'], 'descr' => $lang['archive:cacheExpire#desc'], 'type' => 'input', 'value' => intval(pluginGetVariable($plugin,'cacheExpire'))?pluginGetVariable($plugin,'cacheExpire'):'60'));
+array_push($cfg,  array('mode' => 'group', 'title' => $lang['archive:group.cache'], 'entries' => $cfgX));
 
 
-// RUN 
+// RUN
 if ($_REQUEST['action'] == 'commit') {
 	// If submit requested, do config save
 	commit_plugin_config_changes($plugin, $cfg);
