a4f04a3bd56653b8f79f49b27a7e21ad4c9b7fa0
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-09-25 19:46:31 +0000

    % Дополнение к плагину comments: теперь при выборе обратного порядка комментариев, новые комментарии будут появляться в начале. Но для этого необходимо задать отдельный DIV элемент с ID 'new_comments_rev' (в него и будут попадать комментарии)

--- ng_plugins/trunk/comments/comments.php
+++ ng_plugins/trunk/comments/comments.php
@@ -274,8 +274,9 @@ function plugin_comments_add() {
 				$callingCommentsParams['overrideTemplatePath'] = $templatePath.'/ncustom/'.$ctname;
 		}
 		$output = array(
-			'status' => 1,
-			'data' => iconv('Windows-1251', 'UTF-8', comments_show($SQLnews['id'], $commentId, $SQLnews['com']+1, $callingCommentsParams))
+			'status'	=> 1,
+			'rev'		=> intval(pluginGetVariable('comments', 'backorder')),
+			'data'		=> iconv('Windows-1251', 'UTF-8', comments_show($SQLnews['id'], $commentId, $SQLnews['com']+1, $callingCommentsParams))
 		);
 
 		print json_encode($output);
