c76bf86f6277d7485177f81b363734161ac3d578
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-29 22:53:59 +0000

    Выложена обновлённая (0.04) версия плагина ads_sape: [0.04][2010-12-30] + Добавлена возможность указания конкретных доменных имён, на которых будут показываться ссылки SAPE.   Применимо для случаев, когда у сайта несколько доменных имён (например, с и без www.) и ссылки надо показывать только на одном.

--- ng_plugins/trunk/ads_sape/ads_sape.php
+++ ng_plugins/trunk/ads_sape/ads_sape.php
@@ -4,7 +4,7 @@
 if (!defined('NGCMS')) die ('HAL');
 
 if (!defined('_SAPE_USER')){
-	define('_SAPE_USER', extra_get_param('ads_sape', 'sape_user'));
+	define('_SAPE_USER', pluginGetVariable('ads_sape', 'sape_user'));
 	include_once(root."/plugins/ads_sape/inc/sape.php");
 }
 
@@ -14,11 +14,32 @@ add_act('index_post', 'plugin_ads_sape');
 function plugin_ads_sape() {
 	global $template;
 
-	$count = abs(intval(extra_get_param('ads_sape', 'bcount')));
+	$count = abs(intval(pluginGetVariable('ads_sape', 'bcount')));
 	if (!$count) $count = 1;
 
+	// Check if plugin should be activated on this domain
+	if (trim(pluginGetVariable('ads_sape', 'domains'))) {
+		$found = false;
+		$list = split("\n", pluginGetVariable('ads_sape', 'domains'));
+		foreach ($list as $dn) {
+			if (trim($_SERVER['HTTP_HOST']) == trim($dn)) {
+				$found = true;
+				break;
+			}
+		}
+
+		// Not found. Don't activate plugin
+		if (!$found) {
+			for ($i = 1; $i <= $count; $i++) {
+				$template['vars']['plugin_ads_sape_'.$i] = '';
+			}
+			return;
+		}
+	}
+	print "Loading SAPE ...";
+
 	$blen = array();
-	foreach (split(",", extra_get_param('ads_sape', 'blength')) as $br) {
+	foreach (split(",", pluginGetVariable('ads_sape', 'blength')) as $br) {
 		$blen []= intval(trim($br));
 	}
 
@@ -26,10 +47,10 @@ function plugin_ads_sape() {
 	$blen[$count] = 0;
 
 	$sape = new SAPE_client(array(
-		'multi_site' => extra_get_param('ads_sape', 'multisite')?true:false,
+		'multi_site' => pluginGetVariable('ads_sape', 'multisite')?true:false,
 		'db_dir' => get_plugcache_dir('ads_sape'),
 	));
-	
+
 	for ($i = 1; $i <= $count; $i++) {
 		if ($flag_final) {
 			$template['vars']['plugin_ads_sape_'.$i] = '';
@@ -41,5 +62,5 @@ function plugin_ads_sape() {
 				$template['vars']['plugin_ads_sape_'.$i] = $sape->return_links();
 			}
 		}
-	}	
+	}
 }
