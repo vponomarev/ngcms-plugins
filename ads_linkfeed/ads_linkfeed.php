97c791a1d9aff28c688e1f361a87bff13e9de2ea
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-30 16:29:03 +0000

    Выложена обновлённая (0.02) версия плагина linkfeed: [0.02][2010-12-30] + Добавлена возможность указания конкретных доменных имён, на которых будут показываться ссылки LINKFEED.   Применимо для случаев, когда у сайта несколько доменных имён (например, с и без www.) и ссылки надо показывать только на одном.

--- ads_linkfeed/ads_linkfeed.php
+++ ads_linkfeed/ads_linkfeed.php
@@ -4,7 +4,7 @@
 if (!defined('NGCMS')) die ('HAL');
 
 if (!defined('LINKFEED_USER')){
-	define('LINKFEED_USER', extra_get_param('ads_linkfeed', 'linkfeed_user'));
+	define('LINKFEED_USER', pluginGetVariable('ads_linkfeed', 'linkfeed_user'));
 	include_once(root."/plugins/ads_linkfeed/inc/linkfeed.php");
 }
 
@@ -14,11 +14,31 @@ add_act('index_post', 'plugin_ads_linkfeed');
 function plugin_ads_linkfeed() {
 	global $template;
 
-	$count = abs(intval(extra_get_param('ads_linkfeed', 'bcount')));
+	$count = abs(intval(pluginGetVariable('ads_linkfeed', 'bcount')));
 	if (!$count) $count = 1;
 
+	// Check if plugin should be activated on this domain
+	if (trim(pluginGetVariable('ads_linkfeed', 'domains'))) {
+		$found = false;
+		$list = split("\n", pluginGetVariable('ads_sape', 'domains'));
+		foreach ($list as $dn) {
+			if (trim($_SERVER['HTTP_HOST']) == trim($dn)) {
+				$found = true;
+				break;
+			}
+		}
+
+		// Not found. Don't activate plugin
+		if (!$found) {
+			for ($i = 1; $i <= $count; $i++) {
+				$template['vars']['plugin_ads_sape_'.$i] = '';
+			}
+			return;
+		}
+	}
+
 	$blen = array();
-	foreach (split(",", extra_get_param('ads_linkfeed', 'blength')) as $br) {
+	foreach (split(",", pluginGetVariable('ads_linkfeed', 'blength')) as $br) {
 		$blen []= intval(trim($br));
 	}
 
@@ -26,10 +46,10 @@ function plugin_ads_linkfeed() {
 	$blen[$count] = 0;
 
 	$linkfeed = new LinkfeedClient(array(
-		'multi_site' => extra_get_param('ads_linkfeed', 'multisite')?true:false,
+		'multi_site' => pluginGetVariable('ads_linkfeed', 'multisite')?true:false,
 		'db_dir' => get_plugcache_dir('ads_linkfeed'),
 	));
-	
+
 	for ($i = 1; $i <= $count; $i++) {
 		if ($flag_final) {
 			$template['vars']['plugin_ads_linkfeed_'.$i] = '';
@@ -41,5 +61,5 @@ function plugin_ads_linkfeed() {
 				$template['vars']['plugin_ads_linkfeed_'.$i] = $linkfeed->return_links();
 			}
 		}
-	}	
+	}
 }
