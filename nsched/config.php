5513d46b8274da4c644a7ad97627e328431aba3b
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-08-02 21:45:06 +0000

    % Функционал плагина cron перенесён в ядро (создан новый класс cronManager), в админ панели создан новый раздел "планировщик задач", плагин cron удалён из SVN % Плагин nsched адаптирован к новому функционалу планировщика задач и теперь при своей работе учитывает права пользователей + Добавлена таблица ng_syslog для хранения журнала изменений в системе, срок хранения данных - 30 дней. Опция включается через "настройки системы" => "безопасность" => "Сохранение журнала последних действий пользователей" + Добавлена таблица ng_load для хранения общей информации о нагрузке на сайт. Опция включается через "настройки системы" => "безопасность" => "Сбор данных о нагрузке на сайт", срок хранения данных - 7 дней + Добавлена таблица ng_profiler, используется только в отладочных целях для поиска причин по которым нагруженные сайты могут медленно работать. Опция включается в "настройки системы" => "безопасность" => "Включить детальные SQL логи и логи активностей на XX секунд", там необходимо указать кол-во секунд на которые опция будет активна. При активации в новую таблицу будут попадать данные о всех обращениях пользователей, SQL запросах к БД и данные внутренней отладки CMS. + Добавлена возможность отложенного обновления (каждые 15 минут) счетчика просмотров новостей. Функция крайне полезна для нагруженных сайтов, включается в "настройки системы" => "новости" => "Учёт просмотра новостей" + Обновлён маркер SVN версии

--- nsched/config.php
+++ nsched/config.php
@@ -9,39 +9,37 @@ if (!defined('NGCMS')) die ('HAL');
 // Preload config file
 plugins_load_config();
 
-// Preload engine
-include_once root.'plugins/cron/cron.php';
-
 // Fill configuration parameters
 $cfg = array();
 array_push($cfg, array('descr' => 'Плагин обеспечивает возможность публиковать/снимать с публикации новости по расписанию.'));
-array_push($cfg, array('name' => 'period', 'title' => 'Периодичность анализа новостей', 'descr' => 'Период проверки полей <i>Дата включения</i> и <i>Дата отключения</i>.<br>Чем реже производится анализ - тем ниже нагрузка на БД, но при этом менее точно отрабатывает время публикации/снятия с публикации','type' => 'select', 'values' => array ( '0' => 'не запускать', '5m' => '5 минут', '10m' => '10 минут', '15m' => '15 минут', '30m' => '30 минут', '1h' => '1 час', '2h' => '2 часа', '3h' => '3 часа', '6h' => '6 часов', '12h' => '12 часов'), value => extra_get_param($plugin,'period')));
+array_push($cfg, array('name' => 'period', 'title' => 'Периодичность анализа новостей', 'descr' => 'Период проверки полей <i>Дата включения</i> и <i>Дата отключения</i>.<br>Чем реже производится анализ - тем ниже нагрузка на БД, но при этом менее точно отрабатывает время публикации/снятия с публикации','type' => 'select', 'values' => array ( '0' => 'не запускать', '5m' => '5 минут', '10m' => '10 минут', '15m' => '15 минут', '30m' => '30 минут', '1h' => '1 час', '2h' => '2 часа', '3h' => '3 часа', '6h' => '6 часов', '12h' => '12 часов'), value => pluginGetVariable($plugin,'period')));
 
 // RUN
 if ($_REQUEST['action'] == 'commit') {
 	// If submit requested, do config save
 	//commit_plugin_config_changes($plugin, $cfg);
+
+
 	$regRun = array();
 	switch ($_REQUEST['period']) {
-		case '5m'  : $regRun = array( array('*', '*') ); break;
-		case '10m' : $regRun = array( array('0', '*'), array('10', '*'), array('20', '*'), array('30','*'), array('40','*'), array('50','*')); break;
-		case '15m' : $regRun = array( array('0', '*'), array('15', '*'), array('30', '*'), array('45','*')); break;
-		case '30m' : $regRun = array( array('0', '*'), array('30', '*')); break;
-		case '1h'  : $regRun = array( array('0', '*')); break;
-		case '2h'  : $regRun = array(); for ($i=0; $i<12; $i++) { array_push($regRun, array('0', $i*2)); } break;
-		case '3h'  : $regRun = array(); for ($i=0; $i<8; $i++)  { array_push($regRun, array('0', $i*3)); } break;
-		case '4h'  : $regRun = array(); for ($i=0; $i<6; $i++)  { array_push($regRun, array('0', $i*4)); } break;
-		case '6h'  : $regRun = array(); for ($i=0; $i<4; $i++)  { array_push($regRun, array('0', $i*6)); } break;
-		case '8h'  : $regRun = array(); for ($i=0; $i<3; $i++)  { array_push($regRun, array('0', $i*8)); } break;
-		case '12h' : $regRun = array(); for ($i=0; $i<2; $i++)  { array_push($regRun, array('0', $i*12)); } break;
+		case '5m'  : $regRun = array('*', '*'); break;
+		case '10m' : $regRun = array('0,10,20,30,40,50', '*'); break;
+		case '15m' : $regRun = array('0,15,30,45', '*'); break;
+		case '30m' : $regRun = array('0,30', '*'); break;
+		case '1h'  : $regRun = array('0', '*'); break;
+		case '2h'  : $regRun = array('0', '0,2,4,6,8,10,12,14,16,18,20,22'); break;
+		case '3h'  : $regRun = array('0', '0,3,6,9,12,15,18,21'); break;
+		case '4h'  : $regRun = array('0', '0,4,8,12,16,20'); break;
+		case '6h'  : $regRun = array('0', '0,6,12,18'); break;
+		case '8h'  : $regRun = array('0', '0,8,16'); break;
+		case '12h' : $regRun = array('0', '0,12'); break;
+		default	   : $regRun = array('0', '0'); break;
 	}
 
 	commit_plugin_config_changes($plugin, $cfg);
-	cron_unregister_task('nsched');
-	foreach ($regRun as $v) {
-		cron_register_task('nsched', 'run', $v[0], $v[1], '*', '*', '*');
-	}
-	cron_save();
+
+	$cron->unregisterTask('nsched');
+	$cron->registerTask('nsched', 'run', $regRun[0], $regRun[1], '*', '*', '*');
 
 	print_commit_complete($plugin);
 } else {
--- nsched/nsched.php
+++ nsched/nsched.php
@@ -14,27 +14,72 @@ if (!defined('NGCMS')) die ('HAL');
 
 class NSchedNewsFilter extends NewsFilter {
 	function addNewsForm(&$tvars) {
-		$tvars['plugin']['nsched']  = '<tr><td width="100%" class="contentHead"><img src="'.admin_url.'/skins/default/images/nav.gif" hspace="8" alt="" />Управление публикацией новостей</td></tr>';
-		$tvars['plugin']['nsched'] .= '<tr><td width="100%" class="contentEntry1"><table><tr><td>Дата включения:</td><td><input name="nsched_activate" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr><tr><td>Дата отключения:</td><td><input name="nsched_deactivate" /></td></tr></table></td></tr>';
+		$perm = checkPermission(array('plugin' => '#admin', 'item' => 'news'), null, array('personal.publish', 'personal.unpublish', 'other.publish', 'other.unpublish'));
+
+		$tvars['plugin']['nsched']  = '';
+		if ($perm['personal.publish'] || $perm['personal.unpublish']) {
+			$tvars['plugin']['nsched'] .= '<tr><td width="100%" class="contentHead"><img src="'.admin_url.'/skins/default/images/nav.gif" hspace="8" alt="" />Управление публикацией новостей</td></tr><tr><td width="100%" class="contentEntry1"><table>';
+			if ($perm['personal.publish']) {
+				$tvars['plugin']['nsched'] .= '<tr><td width="100%" class="contentEntry1"><table><tr><td>Дата включения:</td><td><input name="nsched_activate" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr>';
+			}
+			if ($perm['personal.unpublish']) {
+				$tvars['plugin']['nsched'] .= '<tr><td>Дата отключения:</td><td><input name="nsched_deactivate" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr>';
+			}
+			$tvars['plugin']['nsched'] .= '</table></td></tr>';
+
+		}
+
 		return 1;
 	}
 	function addNews(&$tvars, &$SQL) {
-	        $SQL['nsched_activate']   = $_REQUEST['nsched_activate'];
-		$SQL['nsched_deactivate'] = $_REQUEST['nsched_deactivate'];
+		$perm = checkPermission(array('plugin' => '#admin', 'item' => 'news'), null, array('personal.publish', 'personal.unpublish', 'other.publish', 'other.unpublish'));
+		if ($perm['personal.publish'])
+			$SQL['nsched_activate']   = $_REQUEST['nsched_activate'];
+
+		if ($perm['personal.unpublish'])
+			$SQL['nsched_deactivate'] = $_REQUEST['nsched_deactivate'];
 		return 1;
 	}
 	function editNewsForm($newsID, $SQLold, &$tvars) {
+		global $userROW;
+
+		$perm = checkPermission(array('plugin' => '#admin', 'item' => 'news'), null, array('personal.publish', 'personal.unpublish', 'other.publish', 'other.unpublish'));
+		$isOwn = ($SQLold['author_id'] == $userROW['id'])?1:0;
+		$permGroupMode = $isOwn?'personal':'other';
+
 		$ndeactivate = $SQLold['nsched_deactivate'];
 		$nactivate   = $SQLold['nsched_activate'];
 		if ($nactivate   == '0000-00-00 00:00:00') { $nactivate   = ''; }
 		if ($ndeactivate == '0000-00-00 00:00:00') { $ndeactivate = ''; }
-		$tvars['plugin']['nsched']  = '<tr><td width="100%" class="contentHead"><img src="'.admin_url.'/skins/default/images/nav.gif" hspace="8" alt="" />Управление публикацией новостей</td></tr>';
-		$tvars['plugin']['nsched'] .= '<tr><td width="100%" class="contentEntry1"><table><tr><td>Дата включения:</td><td><input name="nsched_activate" value="'.$nactivate.'" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr><tr><td>Дата отключения:</td><td><input name="nsched_deactivate" value="'.$ndeactivate.'" /></td></tr></table></td></tr>';
+
+		$tvars['plugin']['nsched']  = '';
+		if ($perm[$permGroupMode.'.publish'] || $perm[$permGroupMode.'.unpublish']) {
+			$tvars['plugin']['nsched'] .= '<tr><td width="100%" class="contentHead"><img src="'.admin_url.'/skins/default/images/nav.gif" hspace="8" alt="" />Управление публикацией новостей</td></tr><tr><td width="100%" class="contentEntry1"><table>';
+
+			if ($perm[$permGroupMode.'.publish']) {
+				$tvars['plugin']['nsched'] .= '<tr><td>Дата включения:</td><td><input name="nsched_activate" value="'.$nactivate.'" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr>';
+			}
+
+			if ($perm[$permGroupMode.'.unpublish']) {
+				$tvars['plugin']['nsched'] .= '<tr><td>Дата отключения:</td><td><input name="nsched_deactivate" value="'.$ndeactivate.'" /> <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small></td></tr>';
+			}
+			$tvars['plugin']['nsched'] .= '</table></td></tr>';
+		}
 		return 1;
 	}
 	function editNews($newsID, $SQLold, &$SQLnew, &$tvars) {
-	        $SQLnew['nsched_activate']   = $_REQUEST['nsched_activate'];
-		$SQLnew['nsched_deactivate'] = $_REQUEST['nsched_deactivate'];
+		global $userROW;
+
+		$perm = checkPermission(array('plugin' => '#admin', 'item' => 'news'), null, array('personal.publish', 'personal.unpublish', 'other.publish', 'other.unpublish'));
+		$isOwn = ($SQLold['author_id'] == $userROW['id'])?1:0;
+		$permGroupMode = $isOwn?'personal':'other';
+
+		if ($perm[$permGroupMode.'.publish'])
+			$SQLnew['nsched_activate']   = $_REQUEST['nsched_activate'];
+
+		if ($perm[$permGroupMode.'.unpublish'])
+			$SQLnew['nsched_deactivate'] = $_REQUEST['nsched_deactivate'];
+
 		return 1;
 	}
 
@@ -43,14 +88,13 @@ class NSchedNewsFilter extends NewsFilter {
 
 register_filter('news','nsched', new NSchedNewsFilter);
 
-add_act('cron_nsched', 'plugin_nsched');
+//add_act('cron_nsched', 'plugin_nsched');
 
 //
 // Функция вызываемая по крону
 //
-function plugin_nsched() {
+function plugin_nsched_cron() {
 	global $mysql, $catz, $catmap;
-
 
 	// Список новостей для (де)активации
 	$listActivate   = array();
