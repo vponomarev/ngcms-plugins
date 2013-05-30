404c1b09c86e5d84f3dbbfbf2997369c81cfc74f
ngcms <ngcms@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-05-30 22:53:26 +0000

    Выложена обновлённая (0.03) версия плагина ublog
    [0.03][2013-05-29]
    % Плагин адаптирован для обновлённой версии плагина uprofile
    % При попытке зайти на страницу несуществующего блога теперь выводится ошибка
    + Плагину добавлена поддержка собственного шаблона

--- ublog/ublog.php
+++ ublog/ublog.php
@@ -13,7 +13,7 @@ class UblogFilter extends p_uprofileFilter {
 	function showProfile($userID, $SQLrow, &$tvars) {
 		$link = generatePluginLink('ublog', null, array('uid' => $userID, 'uname' => $SQLrow['name']));
 		if (pluginGetVariable('ublog','replaceCount') && ($SQLrow['news'] > 0)) {
-			$tvars['vars']['news'] = '<a href="'.$link.'">'.$SQLrow['news'].'</a>';
+			$tvars['user']['news'] = '<a href="'.$link.'">'.$SQLrow['news'].'</a>';
 			$tvars['vars']['p']['ublog']['flags']['haveBlog'] = true;
 			$tvars['vars']['p']['ublog']['blogLink'] = $link;
 		} else {
@@ -30,7 +30,7 @@ register_plugin_page('ublog','','plugin_ublog');
 
 
 function plugin_ublog() {
-	global $catz, $catmap, $mysql, $config, $userROW, $tpl, $parse, $template, $lang, $PFILTERS, $SYSTEM_FLAGS, $CurrentHandler;
+	global $catz, $catmap, $mysql, $config, $userROW, $tpl, $twig, $twigLoader, $parse, $template, $lang, $PFILTERS, $SYSTEM_FLAGS, $CurrentHandler;
 
 
 	// PREPARE FILTER RULES FOR NEWS SHOWER
@@ -38,9 +38,9 @@ function plugin_ublog() {
 	$pparams = array();
 
 	// preload plugins
-	load_extras('news');
-	load_extras('news:show');
-	load_extras('news:show:list');
+	loadActionHandlers('news');
+	loadActionHandlers('news:show');
+	loadActionHandlers('news:show:list');
 
 	if (isset($CurrentHandler['params']['uid'])) {
 		$filter = array('DATA', 'author_id', '=', intval($CurrentHandler['params']['uid']));
@@ -55,7 +55,8 @@ function plugin_ublog() {
 		$pparams['uname'] = $_GET['uname'];
 		$filter = array('DATA', 'author', '=', $_GET['uname']);
 	} else {
-		$template['vars']['mainblock'] = 'No user specified';
+		// No user is specified - return an error
+		error404();
 		return;
 	}
 
@@ -65,9 +66,47 @@ function plugin_ublog() {
 	} else if (isset($pparams['uname']) && ($urow = $mysql->record("select * from ".uprefix."_users where name = ".db_squote($pparams['uname'])))) {
 		// found :)
 	} else {
-		$template['vars']['mainblock'] = 'User not found';
+		// User not found - return an error
+		error404();
 		return;
 	}
+
+	// Get user's photo and avatar
+	$userPhoto	= userGetPhoto($urow);
+	$userAvatar	= userGetAvatar($urow);
+
+	// Prepare variables
+	$tVars = array(
+		'userRec'		=> $urow,
+		'user'			=> array(
+			'id'			=>	$urow['id'],
+			'name'			=>	$urow['name'],
+			'news'			=>	$urow['news'],
+			'com'			=>	$urow['com'],
+			'status'		=>	$status,
+			'last'			=>	($urow['last'] > 0) ? LangDate("l, j Q Y - H:i", $urow['last']) : $lang['no_last'],
+			'reg'			=>	langdate("j Q Y", $urow['reg']),
+			'site'			=>	secure_html($urow['site']),
+			'icq'			=>	secure_html($urow['icq']),
+			'from'			=>	secure_html($urow['where_from']),
+			'info'			=>	secure_html($urow['info']),
+			'photo_thumb'	=>	$userPhoto[1],
+			'photo'			=>	$userPhoto[2],
+			'avatar'		=>	$userAvatar[1],
+			'flags'			=> array(
+				'hasPhoto'		=> $config['use_photos'] && $userPhoto[0],
+				'hasAvatar'		=> $config['use_avatars'] && $userAvatar[0],
+				'hasIcq'		=> is_numeric($urow['icq'])?1:0,
+			),
+		),
+
+	);
+
+	// [MANUALLY] call xfields plugin for generating user's profile
+	if (is_array($PFILTERS['plugin.uprofile']) && isset($PFILTERS['plugin.uprofile']['xfields']))
+		 { $PFILTERS['plugin.uprofile']['xfields']->showProfile($urow['id'], $urow, $tVars); }
+
+
 
 	$SYSTEM_FLAGS['info']['title']['group'] = str_replace('{uname}', $urow['name'], $lang['ublog:header']);
 
@@ -79,7 +118,9 @@ function plugin_ublog() {
 	$callingParams = array(
 		'style' => 'short',
 		'searchFlag' => true,
-		'extendedReturn' => false,
+		'extendedReturn' => true,
+		'extendedReturnData' => true,
+		'extendedReturnPagination' => true,
 		'customCategoryTemplate' => false,
 		'showNumber'	=> $showNumber,
 		'page'	=> ((isset($_GET['page']) && (intval($_GET['page'])>0))?intval($_GET['page']):0),
@@ -91,5 +132,17 @@ function plugin_ublog() {
 	$paginationParams['xparams'] = array();
 	$paginationParams['paginator'] = array('page', 1, 1);
 
-	$template['vars']['mainblock'] .= news_showlist($filter, $paginationParams, $callingParams);
+	$newsResults = news_showlist($filter, $paginationParams, $callingParams);
+
+	$tVars['news'] = array(
+		'count'		=> $newsResults['count'],
+		'entries'	=> $newsResults['data'],
+	);
+	$tVars['pages']	= $newsResults['pages'];
+
+	$tpath = locatePluginTemplates(array('ublog'), 'ublog', pluginGetVariable('calendar', 'localsource'));
+	$xt = $twig->loadTemplate($tpath['ublog'].'ublog.tpl');
+	$output = $xt->render($tVars);
+
+	$template['vars']['mainblock'] .= $output;
 }
