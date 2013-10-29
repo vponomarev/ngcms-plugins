2fc037952f7317a9ff91cba3509a6b54ccc180f3
stdex <stdex@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-10-29 19:22:50 +0000

    Выложен новый плагин top_active_users (версия 0.4) Плагин позволяет вывести на сайте топ самых активных пользователей.

---
+++ ng_plugins/trunk/top_active_users/config.php
@@ -0,0 +1,36 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+//
+// Configuration file for plugin
+//
+
+// Preload config file
+plugins_load_config();
+LoadPluginLang($plugin, 'config', '', '', ':');
+//LoadPluginLang($plugin, 'main', '', 'topusers');
+
+
+// Fill configuration parameters
+$cfg = array();
+$cfgX = array();
+//array_push($cfg, array('descr' => $lang['topusers_descr']));
+array_push($cfg, array('descr' => $lang['top_active_users:description']));
+
+$cfgX = array();
+array_push($cfgX, array('name' => 'localsource', 'title' => $lang['top_active_users:localsource'], 'descr' => $lang['top_active_users:localsource#desc'], 'type' => 'select', 'values' => array ( '0' => 'Шаблон сайта', '1' => 'Плагин'), 'value' => intval(pluginGetVariable($plugin,'localsource'))));
+array_push($cfg,  array('mode' => 'group', 'title' => $lang['top_active_users:group.source'], 'entries' => $cfgX));
+
+// RUN
+if ($_REQUEST['action'] == 'commit') {
+	// If submit requested, do config save
+	commit_plugin_config_changes($plugin, $cfg);
+	print_commit_complete($plugin);
+} else {
+	generate_config_page($plugin, $cfg);
+}
+
+
+?>
