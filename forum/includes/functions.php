397d6e80d29db67024f8a8fd4820914ffadb358f
ROZARD <ROZARD@e0e46c49-be69-4f5a-ad62-21024a331aea>
2014-06-13 13:04:22 +0000

    Плагин форум: Небольшие правки.

--- forum/includes/functions.php
+++ forum/includes/functions.php
@@ -259,168 +259,6 @@ function update_lastdate_user($update = false)
 		$mysql->query('UPDATE '.prefix.'_users SET last = '.intval($time).' WHERE id = '.intval($userROW['id']));
 }
 
-function check_online_forum()
-{global $mysql, $config, $userROW, $ip, $online, $ipis, $CurrentHandler;
-	
-	if(!pluginGetVariable('forum','online')) return;
-	
-	$time = time() + ($config['date_adjust'] * 60);
-	$last_time = time() + ($config['date_adjust'] * 60) - pluginGetVariable('forum', 'online_time');
-	
-	$bot = forum_filter_bots($_SERVER['HTTP_USER_AGENT']);
-	
-	if( is_array($userROW) ){
-		$id = null;
-		$ips = null;
-		$users = $userROW['name'];
-		$users_id = $userROW['id'];
-		$users_status = $userROW['status'];
-		$location = $CurrentHandler['handlerName'];
-	} elseif($bot){
-		$id = md5($bot);
-		$ips = $ip;
-		$users = $bot;
-		$users_id = 0;
-		$users_status = 5;
-		$location = $CurrentHandler['handlerName'];
-	} else {
-		$id = session_id();
-		$ips = $ip;
-		$users = 'Гость';
-		$users_id = 0;
-		$users_status = 0;
-		$location = $CurrentHandler['handlerName'];
-	}
-	
-	$date_today = date('Y-m-d', $time);
-	$online = array();
-	if(file_exists(FORUM_CACHE.'/online.php'))
-		include(FORUM_CACHE.'/online.php');
-	else
-		file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export(array(), true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-	
-	//print "<pre>".var_export($location, true)."</pre>";
-	
-	if(is_array($online)){
-		$user = array(); $sess = array(); $ipis = array(); $count = count($online);
-		if(isset($count) && ($count > 20000)){
-			$x = 1;
-			while ($count >= 0){
-				$res = $online[$count];
-				if ((isset($res['users_id']) && ($res['users_id'] == 0)) && (isset($res['last_time']) && ($res['last_time'] < $last_time))){
-					array_splice($online, $count, 1);
-					if($x >= 10000) break;
-					$x++;
-				}
-				$count--;
-			}
-			array_splice($online, $count, 1);
-			file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-		}
-		
-		if($date_update < $date_today){
-			while ($count >= 0){
-				$res = $online[$count];
-				if ((isset($res['users_id']) && ($res['users_id'] == 0))){
-					array_splice($online, $count, 1);
-				}
-				$count--;
-			}
-			file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-		}
-		
-		foreach ($online as $row){
-			$user[$row['users_id']] = $row['last_time'];
-			$sess[$row['sess_id']] = $row['last_time'];
-			$ipis[$row['ip']] = $row['last_time'];
-		}
-		
-		if( is_array($userROW) ){
-			if($user[$userROW['id']]){
-				while ($count >= 0){
-					$res = $online[$count];
-					if (isset($res['users_id']) && ($res['users_id'] == $userROW['id'])){
-						$online[$count] = array('sess_id' => $res['sess_id'], 'last_time' => $res['last_time'], 'ip' => $res['ip'], 'users' => $res['users'], 'users_id' => $res['users_id'], 'users_status' => $res['users_status'], 'location' => $location);
-						file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-					}
-					$count--;
-				}
-				if($user[$userROW['id']] < $last_time){
-					$mysql->query('UPDATE '.prefix.'_users SET last = '.securemysql($user[$userROW['id']]).' WHERE id = '.securemysql($userROW['id']));
-					while ($count >= 0){
-						$res = $online[$count];
-						if (isset($res['users_id']) && ($res['users_id'] == $userROW['id'])){
-							$online[$count] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-							file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-						}
-						$count--;
-					}
-				}
-			}elseif($ipis[$ip]){
-				while($count >= 0){
-					$res = $online[$count];
-					if (isset($res['ip']) && ($res['ip'] == $ip)){
-						$online[$count] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-						file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-					}
-					$count--;
-				}
-			} else {
-				$online[] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-				file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-			}
-		} else {
-			if($sess[$id]){
-				while ($count >= 0){
-					$res = $online[$count];
-					if (isset($res['sess_id']) && ($res['sess_id'] == $id)){
-						$online[$count] = array('sess_id' => $res['sess_id'], 'last_time' => $res['last_time'], 'ip' => $res['ip'], 'users' => $res['users'], 'users_id' => $res['users_id'], 'users_status' => $res['users_status'], 'location' => $location);
-						file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-					}
-					$count--;
-				}
-				if($sess[$id] < $last_time){
-					while ($count >= 0){
-						$res = $online[$count];
-						if (isset($res['sess_id']) && ($res['sess_id'] == $id)){
-							$online[$count] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-							file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-						}
-						$count--;
-					}
-				}
-			} else {
-				if( $ipis[$ip] ){
-					while ($count >= 0){
-						$res = $online[$count];
-						if (isset($res['ip']) && ($res['ip'] == $ip)){
-							$online[$count] = array('sess_id' => $res['sess_id'], 'last_time' => $res['last_time'], 'ip' => $res['ip'], 'users' => $res['users'], 'users_id' => $res['users_id'], 'users_status' => $res['users_status'], 'location' => $location);
-							file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-						}
-						$count--;
-					}
-					if($ipis[$ip] < $last_time){
-						while ($count >= 0){
-							$res = $online[$count];
-							if (isset($res['ip']) && ($res['ip'] == $ip)){
-								$online[$count] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-								file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';'); break;
-							}
-							$count--;
-						}
-					}
-				} else {
-					$online[] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-					file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-				}
-			}
-		}
-	} else {
-		$online[] = array('sess_id' => $id, 'last_time' => $time, 'ip' => $ips, 'users' => $users, 'users_id' => $users_id, 'users_status' => $users_status, 'location' => $location);
-		file_put_contents(FORUM_CACHE.'/online.php', '<?php'."\n\n".'$online = '.var_export($online, true).';'."\n\n".'$date_update = \''.$date_today.'\';');
-	}
-}
-
 function rss_export_generate_forum($tid = ''){
 global $config, $mysql, $show_main, $twig;
 	
@@ -486,12 +324,12 @@ function suppress_show()
 }
 
 function banned_users()
-{global $ip, $ban;
+{//global $ip, $ban;
 	
-	if(file_exists(FORUM_CACHE.'/ban.php'))
-		include(FORUM_CACHE.'/ban.php');
-	else
-		file_put_contents(FORUM_CACHE.'/ban.php', '<?php'."\n\n".'$ban = '.var_export(array(), true).';'."\n\n");
+//	if(file_exists(FORUM_CACHE.'/ban.php'))
+//		include(FORUM_CACHE.'/ban.php');
+//	else
+//		file_put_contents(FORUM_CACHE.'/ban.php', '<?php'."\n\n".'$ban = '.var_export(array(), true).';'."\n\n");
 }
 
 function lang_forum($lang){
@@ -582,23 +420,6 @@ function forum_memory_usage(){
 		return round( memory_get_usage() / 1024  / 1024, 2) . 'MB';
 	else
 		return '<b>Неизвестно</b>';
-}
-
-
-function forum_filter_bots($u_agent){
-	$engines = array(
-		'YandexBot' => 'YandexBot',
-		'YandexDirect' => 'YandexDirect',
-		'Googlebot' => 'Googlebot',
-	);
-	
-	foreach ($engines as $key => $value){
-		if (stripos($u_agent, $key)){
-			return($value);
-		}
-	}
-	
-	return false;
 }
 
 function redirect_forum($url){
---
+++ forum/tpl/config/ban.tpl
@@ -0,0 +1,67 @@
+<table border="0" cellspacing="0" cellpadding="0" width="100%">
+	<tr align="center">
+		<td width="100%" class="contentNav" align="center" style="background-repeat: no-repeat; background-position: left;">
+			<input type="button" onmousedown="javascript:window.location.href='{{ admin_url }}/admin.php?mod=extra-config&plugin=forum&action=ban'" value="Общие" class="navbutton" />
+			<input type="button" onmousedown="javascript:window.location.href='{{ admin_url }}/admin.php?mod=extra-config&plugin=forum&action=ban_ip_list'" value="Забаненые IP" class="navbutton" />
+			<input type="button" onmousedown="javascript:window.location.href='{{ admin_url }}/admin.php?mod=extra-config&plugin=forum&action=ban_ip_range'" value="Забаненый диапазон IP" class="navbutton" />
+			<input type="button" onmousedown="javascript:window.location.href='{{ admin_url }}/admin.php?mod=extra-config&plugin=forum&action=ban_user'" value="Забаненые пользователи" class="navbutton" />
+		</td>
+	</tr>
+</table><br />
+<form method="post" action="">
+<tr>
+<td colspan=2>
+<fieldset class="admGroup">
+<legend class="title"><b>Настройки форума</b></legend>
+<table width="100%" border="0" class="content">
+<tr>
+<td class="contentEntry1" valign=top>Выберите каталог из которого плагин будет брать шаблоны для отображения<br /></td>
+<td class="contentEntry2" valign=top>{{ localsource }}</td>
+
+</tr><tr>
+<td class="contentEntry1" valign=top>Выберите вид отображения форума<br /></td>
+<td class="contentEntry2" valign=top>{{ display_main }}</td>
+
+</tr><tr>
+<td class="contentEntry1" valign=top>Время для редиректа<br /><small>Устанавливать в секундах...</small></td>
+<td class="contentEntry2" valign=top><input name="redirect_time" type="text" title="Время для редиректа" size="4" value="{{ redirect_time }}" /></td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Время в течении которого пользователь считается в сети<br /><small>Указывать в секундах</small></td>
+<td class="contentEntry2" valign=top><input name="online_time" type="text" title="Время в течении которого пользователь считается в сети" size="10" value="{{ online_time }}" /></td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Включить кто оналайн<br /></td>
+
+<td class="contentEntry2" valign=top>{{ online }}</td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Титл форума<br /></td>
+<td class="contentEntry2" valign=top><input name="forum_title" type="text" title="Титл форума"  value="{{ forum_title }}" /></td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Описание форума<br /></td>
+<td class="contentEntry2" valign=top><input name="forum_description" type="text" title="Описание форума"  value="{{ forum_description }}" /></td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Ключевые слова<br /></td>
+
+<td class="contentEntry2" valign=top><input name="forum_keywords" type="text" title="Ключевые слова"  value="{{ forum_keywords }}" /></td>
+</tr><tr>
+<td class="contentEntry1" valign=top>Выберите активный шаблон<br /><small>Выбранный скин будет использоваться при установке <b>Плагин</b> в предыдущем поле</small><br /></td>
+<td class="contentEntry2" valign=top>{{ localskin }}</td>
+</tr>
+<tr>
+<td class="contentEntry1" valign=top>Укажите время в течении которого можно редактировать и удалять сообщения<br /><small></small><br /></td>
+<td class="contentEntry2" valign=top><input name="edit_del_time" type="text" title="Укажите время в течении которого можно редактировать и удалять сообщения"  value="{{ edit_del_time }}" /></td>
+</tr>
+</table>
+</fieldset>
+</td>
+</tr>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr>
+	<td width="100%" colspan="2">&nbsp;</td>
+</tr>
+<tr align="center">
+	<td width="100%" colspan="2" class="contentEdit" align="center">
+		<input name="submit" type="submit"  value="Сохранить" class="button" />
+	</td>
+</tr>
+</table>
+</form>
