82a24fb816d18295ee7b3bb2cf8e599ba108d011
RostunovSergey <sergey@rostunov.com>
2016-01-04 23:38:32 +0300

    [0.11][2016-01-04] % Добавлен API для добавления страниц из других плагинов % Добавлен URL в ЧПУ при установке

---
+++ gsmg/install.php
@@ -0,0 +1,39 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+plugins_load_config();
+
+// Load library
+include_once(root."/plugins/gsmg/lib/common.php");
+
+//
+// Configuration file for plugin
+//
+
+//
+// Install script for plugin.
+// $action: possible action modes
+//  confirm     - screen for installation confirmation
+//  apply       - apply installation, with handy confirmation
+//  autoapply       - apply installation in automatic mode [INSTALL script]
+//
+function plugin_gsmg_install($action) {
+    
+    // Apply requested action
+    switch ($action) {
+        case 'confirm':
+            generate_install_page('gsmg', '');
+            break;
+        case 'autoapply':
+        case 'apply':
+            create_urls();
+            plugin_mark_installed('gsmg');
+            $url = home."/engine/admin.php?mod=extras";
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: {$url}");
+            break;
+    }
+    return true;
+}
---
+++ gsmg/uninstall.php
@@ -0,0 +1,23 @@
+<?php
+
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+
+// Load library
+include_once(root."/plugins/gsmg/lib/common.php");
+
+//
+// Configuration file for plugin
+//
+
+plugins_load_config();
+
+if ($_REQUEST['action'] == 'commit') {
+    remove_urls();
+    plugin_mark_deinstalled($plugin);
+    $url = home."/engine/admin.php?mod=extras";
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: {$url}");
+} else {
+    generate_install_page($plugin, 'Удаление плагина', 'deinstall');
+}
