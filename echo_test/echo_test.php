89430fed13adbbdbce7984baa495e024990390f9
stdex <sergey@rostunov.com>
2017-02-07 22:32:39 +0300

    Добавлен плагин echo_test

---
+++ echo_test/echo_test.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('NGCMS'))
+    die ('HAL');
+
+add_act('index', 'plugin_echo_test');
+
+function plugin_echo_test()
+{
+    global $template;
+    $echo = 'произвольный текст';
+    $template['vars']['echo_test_var1'] = $echo;
+}
