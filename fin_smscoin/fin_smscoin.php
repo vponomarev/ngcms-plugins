bca2ae2ac92f98e73c3dc462e83fada8c64e527a
stdex <sergey@rostunov.com>
2018-05-24 03:42:14 +0300

    r1

--- fin_smscoin/fin_smscoin.php
+++ fin_smscoin/fin_smscoin.php
@@ -2,18 +2,6 @@
 // Protect against hack attempts
 if (!defined('NGCMS')) die ('HAL');
 include_once(root . "/plugins/finance/inc/finance.php");
-//
-// Add json_decode() support for PHP < 5.2.0
-//
-if (!function_exists('json_decode')) {
-	function json_decode($json, $assoc = false) {
-
-		include_once root . 'includes/classes/json.php';
-		$jclass = new Services_JSON($assoc ? SERVICES_JSON_LOOSE_TYPE : 0);
-
-		return $jclass->decode($json);
-	}
-}
 
 class Finance_Acceptor_SMSCOIN extends Finance_Acceptor {
 
