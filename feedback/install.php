9ef234911075cca2c782dc4111404ac5e86d2926
Рустам <rusiq@list.ru>
2020-07-21 14:49:47 +1000

    [feedback] Форматирование кода PHP с помощью styleci и atom-beautify

--- feedback/install.php
+++ feedback/install.php
@@ -1,6 +1,9 @@
 <?php
+
 // Protect against hack attempts
-if (!defined('NGCMS')) die ('HAL');
+if (!defined('NGCMS')) {
+    die('HAL');
+}
 //
 // Configuration file for plugin
 //
@@ -11,46 +14,47 @@ if (!defined('NGCMS')) die ('HAL');
 //	apply		- apply installation, with handy confirmation
 //	autoapply       - apply installation in automatic mode [INSTALL script]
 //
-function plugin_feedback_install($action) {
+function plugin_feedback_install($action)
+{
+    global $lang;
+    if ($action != 'autoapply') {
+        loadPluginLang('feedback', 'config', '', '', ':');
+    }
+    // Fill DB_UPDATE configuration scheme
+    $db_update = [
+        [
+            'table'  => 'feedback',
+            'action' => 'cmodify',
+            'key'    => 'primary key(id)',
+            'fields' => [
+                ['action' => 'cmodify', 'name' => 'id', 'type' => 'int', 'params' => 'not null auto_increment'],
+                ['action' => 'cmodify', 'name' => 'active', 'type' => 'int', 'params' => 'default 0'],
+                ['action' => 'cmodify', 'name' => 'regonly', 'type' => 'int', 'params' => 'default 0'],
+                ['action' => 'cmodify', 'name' => 'flags', 'type' => 'char(20)'],
+                ['action' => 'cmodify', 'name' => 'name', 'type' => 'char(40)'],
+                ['action' => 'cmodify', 'name' => 'title', 'type' => 'char(80)'],
+                ['action' => 'cmodify', 'name' => 'subj', 'type' => 'char(100)'],
+                ['action' => 'cmodify', 'name' => 'description', 'type' => 'text'],
+                ['action' => 'cmodify', 'name' => 'struct', 'type' => 'text'],
+                ['action' => 'cmodify', 'name' => 'template', 'type' => 'char(50)'],
+                ['action' => 'cmodify', 'name' => 'emails', 'type' => 'text'],
+            ],
+        ],
+    ];
+    // Apply requested action
+    switch ($action) {
+        case 'confirm':
+            generate_install_page('feedback', $lang['feedback:text.install']);
+            break;
+        case 'autoapply':
+        case 'apply':
+            if (fixdb_plugin_install('feedback', $db_update, 'install', ($action == 'autoapply') ? true : false)) {
+                plugin_mark_installed('feedback');
+            } else {
+                return false;
+            }
+            break;
+    }
 
-	global $lang;
-	if ($action != 'autoapply')
-		loadPluginLang('feedback', 'config', '', '', ':');
-	// Fill DB_UPDATE configuration scheme
-	$db_update = array(
-		array(
-			'table'  => 'feedback',
-			'action' => 'cmodify',
-			'key'    => 'primary key(id)',
-			'fields' => array(
-				array('action' => 'cmodify', 'name' => 'id', 'type' => 'int', 'params' => 'not null auto_increment'),
-				array('action' => 'cmodify', 'name' => 'active', 'type' => 'int', 'params' => 'default 0'),
-				array('action' => 'cmodify', 'name' => 'regonly', 'type' => 'int', 'params' => 'default 0'),
-				array('action' => 'cmodify', 'name' => 'flags', 'type' => 'char(20)'),
-				array('action' => 'cmodify', 'name' => 'name', 'type' => 'char(40)'),
-				array('action' => 'cmodify', 'name' => 'title', 'type' => 'char(80)'),
-				array('action' => 'cmodify', 'name' => 'subj', 'type' => 'char(100)'),
-				array('action' => 'cmodify', 'name' => 'description', 'type' => 'text'),
-				array('action' => 'cmodify', 'name' => 'struct', 'type' => 'text'),
-				array('action' => 'cmodify', 'name' => 'template', 'type' => 'char(50)'),
-				array('action' => 'cmodify', 'name' => 'emails', 'type' => 'text'),
-			)
-		),
-	);
-	// Apply requested action
-	switch ($action) {
-		case 'confirm':
-			generate_install_page('feedback', $lang['feedback:text.install']);
-			break;
-		case 'autoapply':
-		case 'apply':
-			if (fixdb_plugin_install('feedback', $db_update, 'install', ($action == 'autoapply') ? true : false)) {
-				plugin_mark_installed('feedback');
-			} else {
-				return false;
-			}
-			break;
-	}
-
-	return true;
+    return true;
 }
--- feedback/uninstall.php
+++ feedback/uninstall.php
@@ -1,22 +1,24 @@
 <?php
+
 // Protect against hack attempts
-if (!defined('NGCMS')) die ('HAL');
+if (!defined('NGCMS')) {
+    die('HAL');
+}
 //
 // Configuration file for plugin
 //
 pluginsLoadConfig();
 loadPluginLang('feedback', 'config', '', '', ':');
-$db_update = array(
-	array(
-		'table'  => 'feedback',
-		'action' => 'drop',
-	),
-);
+$db_update = [
+    [
+        'table'  => 'feedback',
+        'action' => 'drop',
+    ],
+];
 if ($_REQUEST['action'] == 'commit') {
-	if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
-		plugin_mark_deinstalled($plugin);
-	}
+    if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
+        plugin_mark_deinstalled($plugin);
+    }
 } else {
-	generate_install_page($plugin, $lang['feedback:text.deinstall'], 'deinstall');
+    generate_install_page($plugin, $lang['feedback:text.deinstall'], 'deinstall');
 }
-?>
