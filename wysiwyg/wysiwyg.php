a72648747b4948251fb5a545b8bd17a10986f399
Vitaly A Ponomarev <vitaly.ponomarev@gmail.com>
2020-07-31 01:29:40 +0300

    WYSIWYG: Адаптация плагина wysiwyg к текущей GIT версии NGCMS

--- wysiwyg/wysiwyg.php
+++ wysiwyg/wysiwyg.php
@@ -1,82 +1,92 @@
 <?php
 if (!defined('NGCMS')) die ('HAL');
-function bb(&$tvars) {
 
-	$name_bb = pluginGetVariable('wysiwyg', 'bb_editor');
-	$includ_bb = array();
-	$isBBCode = false;
-	switch ($name_bb) {
-		case 'wysibb':
-			$js_array = array();
-			foreach (array(admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/jquery.wysibb.js', admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/wysibb.js') as $value) {
-				$js_array[] = '<script type="text/javascript" src="' . $value . '"></script>';
-			}
-			$css_array = array();
-			foreach (array(admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/theme/default/wbbtheme.css') as $value) {
-				$css_array[] = '<link rel="stylesheet" href="' . $value . '" type="text/css" media="screen" />';
-			}
-			break;
-		case 'jodit';
-			$js_array = array();
-			foreach (array(admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/jodit.min.js', admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/jodit.js') as $value) {
-				$js_array[] = '<script type="text/javascript" src="' . $value . '"></script>';
-			}
-			$css_array = array();
-			foreach (array(admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/jodit.min.css', 'http://fonts.googleapis.com/css?family=Lato:100,300,400,700,100italic,300italic,400italic,700italic') as $value) {
-				$css_array[] = '<link rel="stylesheet" href="' . $value . '" type="text/css" media="screen" />';
-			}
-			break;
-		case 'tinymce':
-			$js_array = array();
-			foreach (array(admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/tinymce.min.js', admin_url . '/plugins/wysiwyg/bb_code/' . $name_bb . '/tinymce.js') as $value) {
-				$js_array[] = '<script type="text/javascript" src="' . $value . '"></script>';
-			}
-			$css_array = array();
-			foreach (array() as $value) {
-				$css_array[] = '<link rel="stylesheet" href="' . $value . '" type="text/css" media="screen" />';
-			}
-			break;
-		default:
-			$array_bb = array();
-			break;
-	}
-	$array_bb = array_merge($js_array, $css_array);
-	if (!is_array($array_bb)) return;
-	$tvars['includ_bb'] = implode($array_bb, "\n");
-	$tvars['attributBB'] = 'bb_code';
-	$tvars['isBBCode'] = true;
+function bb(&$tvars) {
+    $mapping = [
+        'wysibb' => [
+            'js' => [
+                'jquery.wysibb.js',
+                'wysibb.js',
+            ],
+            'css' => [
+                'theme/default/wbbtheme.css',
+            ],
+        ],
+        'jodit' => [
+            'js'    => [
+                'jodit.min.js',
+                'jodit.js',
+            ],
+            'css'   => [
+                'jodit.min.css',
+                ['http://fonts.googleapis.com/css?family=Lato:100,300,400,700,100italic,300italic,400italic,700italic', 'absolute'],
+            ],
+        ],
+        'tinymce' => [
+            'js'    => [
+                'tinymce.min.js',
+                'tinymce.js',
+            ],
+            'css' => [ ],
+        ],
+    ];
+
+    $id = pluginGetVariable('wysiwyg', 'bb_editor');
+    $list = [];
+    if (isset($mapping[$id])) {
+        foreach ($mapping[$id]['js'] as $v) {
+            // Get URL
+            $url = is_array($v)?$v[0]:$v;
+
+            // Add leading part if URL is not absolute
+            if (!is_array($v) || $v[1] != 'absolute') {
+                $url = admin_url.'/plugins/wysiwyg/bb_code/'.$id.'/'.$url;
+            }
+            $list []= '<script type="text/javascript" src="'.$url.'"></script>';
+        }
+
+        foreach ($mapping[$id]['css'] as $v) {
+            // Get URL
+            $url = is_array($v)?$v[0]:$v;
+
+            // Add leading part if URL is not absolute
+            if (!is_array($v) || $v[1] != 'absolute') {
+                $url = admin_url.'/plugins/wysiwyg/bb_code/'.$id.'/'.$url;
+            }
+            $list []= '<link rel="stylesheet" href="'.$url.'" type="text/css" media="screen" />';
+        }
+    }
+    if (!count($list))
+        return;
+
+    $tvars['preloadRAW'] = (isset($tvars['preloadRAW'])?$tvars['preloadRAW']:'').join("\n", $list);
+    $tvars['flags']['disableTagsSmilies'] = true;
+    $tvars['editorClassName'] = 'bb_code';
+    $tvars['isBBCode'] = true;
+    $tvars['attributBB'] = 'bb_code';
+    return;
 }
 
 class wysiwygNewsFilter extends NewsFilter {
-
 	function addNewsForm(&$tvars) {
-
 		bb($tvars);
-
 		return true;
 	}
 
 	function editNewsForm($newsID, $SQLnews, &$tvars) {
-
 		bb($tvars);
-
 		return true;
 	}
 }
 
 class wysiwygStaticFilter extends StaticFilter {
-
 	function addStaticForm(&$tvars) {
-
 		bb($tvars);
-
 		return true;
 	}
 
 	function editStaticForm($staticID, $SQLnews, &$tvars) {
-
 		bb($tvars);
-
 		return true;
 	}
 }
@@ -85,11 +95,8 @@ if (getPluginStatusActive('comments')) {
 	loadPluginLibrary('comments', 'lib');
 
 	class wysiwygFilterComments extends FilterComments {
-
 		function addCommentsForm($newsID, &$tvars) {
-
 			bb($tvars);
-
 			return true;
 		}
 	}
