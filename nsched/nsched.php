8c886a750ff6f82d5c62b7a2fc25193a6092a7e4
russsiq <rusiq@list.ru>
2021-11-11 03:37:39 +1000

    Выделит шаблоны из скриптов PHP в файлы шаблонизатора TWIG

--- nsched/nsched.php
+++ nsched/nsched.php
@@ -18,6 +18,9 @@ class NSchedNewsFilter extends NewsFilter
 {
     public function addNewsForm(&$tvars)
     {
+        /** @var Twig\Environment $twig */
+        global $twig;
+
         $perm = checkPermission(
             [
                 'plugin' => '#admin',
@@ -35,26 +38,15 @@ class NSchedNewsFilter extends NewsFilter
         $tvars['plugin']['nsched'] = '';
 
         if ($perm['personal.publish'] || $perm['personal.unpublish']) {
-            $tvars['plugin']['nsched'] .= '<tbody><tr class="thead-dark">
-                    <th scope="col">Управление публикацией новостей</th>
-                </tr>';
-
-            if ($perm['personal.publish']) {
-                $tvars['plugin']['nsched'] .= '<tr>
-                        <td>Дата включения:</td>
-                        <td><input  id="nsched_activate" name="nsched_activate" class="form-control" pattern="[0-9]{2}\.[0-9]{2}\.[0-9]{4} [0-9]{2}:[0-9]{2}"/> <small>( в формате ГГГГ.ММ.ДД ЧЧ:ММ )</small></td>
-                    </tr>';
-            }
-            if ($perm['personal.unpublish']) {
-                $tvars['plugin']['nsched'] .= '<tr>
-                        <td>Дата отключения:</td>
-                        <td><input  id="nsched_deactivate" name="nsched_deactivate" class="form-control" pattern="[0-9]{2}\.[0-9]{2}\.[0-9]{4} [0-9]{2}:[0-9]{2}"/> <small>( в формате ГГГГ.ММ.ДД ЧЧ:ММ )</small></td>
-                    </tr>';
-            }
-            $tvars['plugin']['nsched'] .= '</tbody>
-                <script language="javascript" type="text/javascript">'
-                    ."$('#nsched_activate').datetimepicker({ dateFormat: 'yy.mm.dd', timeFormat: 'hh:mm'});$('#nsched_deactivate').datetimepicker({ dateFormat: 'yy.mm.dd', timeFormat: 'hh:mm'});
-                </script>";
+            $tvars['plugin']['nsched'] = $twig->render(
+                'plugins/nsched/tpl/add_news.tpl',
+                [
+                    'flags' => [
+                        'can_publish' => (bool) $perm['personal.publish'],
+                        'can_unpublish' => (bool) $perm['personal.unpublish'],
+                    ],
+                ]
+            );
         }
 
         return 1;
@@ -89,7 +81,8 @@ class NSchedNewsFilter extends NewsFilter
 
     public function editNewsForm($newsID, $SQLold, &$tvars)
     {
-        global $userROW;
+        /** @var Twig\Environment $twig */
+        global $twig, $userROW;
 
         $perm = checkPermission(
             [
@@ -110,39 +103,28 @@ class NSchedNewsFilter extends NewsFilter
         $ndeactivate = $SQLold['nsched_deactivate'];
         $nactivate = $SQLold['nsched_activate'];
 
-        if ($nactivate == '0000.00.00 00:00') {
+        if ($nactivate == '0000-00-00 00:00:00') {
             $nactivate = '';
         }
 
-        if ($ndeactivate == '0000.00.00 00:00') {
+        if ($ndeactivate == '0000-00-00 00:00:00') {
             $ndeactivate = '';
         }
 
         $tvars['plugin']['nsched'] = '';
 
         if ($perm[$permGroupMode.'.publish'] || $perm[$permGroupMode.'.unpublish']) {
-            $tvars['plugin']['nsched'] .= '<tbody><tr class="thead-dark">
-                    <th scope="col">Управление публикацией новостей</th>
-                </tr>';
-
-            if ($perm[$permGroupMode.'.publish']) {
-                $tvars['plugin']['nsched'] .= '<tr>
-                        <td>Дата включения:</td>
-                        <td><input  name="nsched_activate" id="nsched_activate" class="form-control" pattern="[0-9]{2}\.[0-9]{2}\.[0-9]{4} [0-9]{2}:[0-9]{2}" value="'.$nactivate.'" /> <small>( в формате ГГГГ.ММ.ДД ЧЧ:ММ )</small></td>
-                    </tr>';
-            }
-
-            if ($perm[$permGroupMode.'.unpublish']) {
-                $tvars['plugin']['nsched'] .= '<tr>
-                        <td>Дата отключения:</td>
-                        <td><input  name="nsched_deactivate" id="nsched_deactivate" class="form-control" pattern="[0-9]{2}\.[0-9]{2}\.[0-9]{4} [0-9]{2}:[0-9]{2}" value="'.$ndeactivate.'" /> <small>( в формате ГГГГ.ММ.ДД ЧЧ:ММ )</small></td>
-                    </tr>';
-            }
-
-            $tvars['plugin']['nsched'] .= '</tbody>
-                <script language="javascript" type="text/javascript">'
-                    ."$('#nsched_activate').datetimepicker({ dateFormat: 'yy.mm.dd', timeFormat: 'hh:mm', currentText: '".$nactivate."'});$('#nsched_deactivate').datetimepicker({ dateFormat: 'yy.mm.dd', timeFormat: 'hh:mm', currentText: '".$ndeactivate."'});
-                </script>";
+            $tvars['plugin']['nsched'] = $twig->render(
+                'plugins/nsched/tpl/edit_news.tpl',
+                [
+                    'nsched_activate' => $nactivate,
+                    'nsched_deactivate' => $ndeactivate,
+                    'flags' => [
+                        'can_publish' => (bool) $perm[$permGroupMode.'.publish'],
+                        'can_unpublish' => (bool) $perm[$permGroupMode.'.unpublish'],
+                    ],
+                ]
+            );
         }
 
         return 1;
---
+++ nsched/tpl/edit_news.tpl
@@ -0,0 +1,41 @@
+{# $today = date("Y-m-d H:i:s"); // 2001-03-10 17:16:18 (the MySQL DATETIME format) #}
+
+<tr>
+    <th colspan="2">Управление публикацией новостей</th>
+</tr>
+
+{% if (flags.can_publish) %}
+<tr>
+    <td>
+        Дата включения:<br />
+        <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small>
+    </td>
+    <td>
+        <input type="text" name="nsched_activate" value="{{ nsched_activate }}" class="form-control" />
+    </td>
+</tr>
+{% endif %}
+
+{% if (flags.can_unpublish) %}
+<tr>
+    <td>
+        Дата отключения:<br />
+        <small>( в формате ГГГГ-ММ-ДД ЧЧ:ММ )</small>
+    </td>
+    <td>
+        <input type="text" name="nsched_deactivate" value="{{ nsched_deactivate }}" class="form-control" />
+    </td>
+</tr>
+{% endif %}
+
+<script>
+    $('[name="nsched_activate"]').datetimepicker({
+        format: 'Y-m-d H:i:s',
+        currentText: '{{ nsched_activate }}',
+    });
+
+    $('[name="nsched_deactivate"]').datetimepicker({
+        format: 'Y-m-d H:i:s',
+        currentText: '{{ nsched_deactivate }}',
+    });
+</script>
